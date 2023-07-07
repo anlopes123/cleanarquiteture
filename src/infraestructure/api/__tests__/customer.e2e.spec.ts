87743b2879e8860b2dac6b112ce2c0fd7a3ffb60
Anizair Lopes <anlopes123@gmail.com>
2023-07-06 22:55:20 -0300

    Feito presenter para customer retornar xml

--- src/infraestructure/api/__tests__/customer.e2e.spec.ts
+++ src/infraestructure/api/__tests__/customer.e2e.spec.ts
@@ -76,6 +76,14 @@ describe("E2E test for customer", ()=>{
         const customer1 = listResponse.body.customers[1];
         expect(customer1.name).toBe("Jame");
         expect(customer1.address.street).toBe("Street 1");
+
+        const listResponseXML = await request(app)
+        .get("/customer")
+        .set("Accept", "application/xml")
+        .send();
+
+        expect(listResponseXML.status).toBe(200);
+        expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
         
     });
 });
